fd6d89c447e5d268132f7f66b94021552c6bacdf
Sudheesh S Babu <sudheeshs.babu@safetywhat.com>
2026-01-13 15:09:15 +0530

    User event test case with test hook beforeEach

--- src/pages/home/Product.jsx
+++ src/pages/home/Product.jsx
@@ -15,15 +15,21 @@ function Product({ product, loadCart }) {
     return (
         <div className="product-container">
             <div className="product-image-container">
-                <img className="product-image"
-                    src={product.image} />
+                <img
+                    className="product-image"
+                    data-testid="product-image"
+                    src={product.image}
+                />
             </div>
             <div className="product-name limit-text-to-2-lines">
                 {product.name}
             </div>
             <div className="product-rating-container">
-                <img className="product-rating-stars"
-                    src={`images/ratings/rating-${(parseFloat(product.rating.stars) * 10).toFixed(0)}.png`} />
+                <img
+                    className="product-rating-stars"
+                    data-testid="product-rating-stars"
+                    src={`images/ratings/rating-${(parseFloat(product.rating.stars) * 10).toFixed(0)}.png`}
+                />
                 <div className="product-rating-count link-primary">
                     {product.ratingCount}
                 </div>
@@ -55,6 +61,7 @@ function Product({ product, loadCart }) {
             </div>
             <button
                 className="add-to-cart-button button-primary"
+                data-testid="add-to-cart-button"
                 onClick={addToCart}
             >
                 Add to Cart
---
+++ src/pages/home/Product.test.jsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import { it, expect, describe, vi, beforeEach } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import Product from './Product';
+import userEvent from '@testing-library/user-event';
+
+vi.mock('axios');
+
+describe('Product component', () => {
+    let product, loadCart;
+    beforeEach(() => {
+        product = {
+            id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+            image: "images/products/intermediate-composite-basketball.jpg",
+            name: "Intermediate Size Basketball",
+            rating: {
+                stars: 4,
+                count: 127
+            },
+            priceCents: 2095,
+            keywords: ["sports", "basketballs"]
+        }
+        loadCart = vi.fn();
+    })
+    it('renders the product details correctly', () => {
+        render(<Product product={product} loadCart={loadCart} />)
+        expect(
+            screen.getByText(product.name)
+        ).toBeInTheDocument()
+        expect(
+            screen.getByText('$20.95')
+        ).toBeInTheDocument()
+        expect(
+            screen.getByTestId('product-image')
+        ).toHaveAttribute('src', product.image)
+        expect(
+            screen.getByTestId('product-rating-stars')
+        ).toHaveAttribute('src', `images/ratings/rating-${(parseFloat(product.rating.stars) * 10).toFixed(0)}.png`);
+
+    });
+    it('adds a product to the cart', async () => {
+        render(<Product product={product} loadCart={loadCart} />)
+        const user = userEvent.setup();
+        const addToCartButton = screen.getByTestId('add-to-cart-button');
+        await user.click(addToCartButton);
+        expect(axios.post).toHaveBeenCalledWith('/api/cart-items', {
+            productId: product.id,
+            quantity: 1
+        })
+        expect(loadCart).toHaveBeenCalled();
+    })
+})
